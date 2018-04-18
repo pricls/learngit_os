dc2ca3dc51e32ef69daee027a583e33fc1ba58b1
slc <csl18715030447@outlook>
2018-04-18 17:45:03 +0800

    xiu zhen

--- python/psutil/md.py
+++ python/psutil/md.py
@@ -12,4 +12,4 @@ print( s )
 # 获取磁盘信息
 print(psutil.disk_partitions()) # 磁盘分区信息
 print(psutil.disk_usage('/')) # 磁盘使用情况
-print(psutil.disk_io_counters())  # 磁盘IO
+print(psutil.disk_io_counters()) # 磁盘IO
